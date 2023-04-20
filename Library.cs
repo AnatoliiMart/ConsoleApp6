586a337b00f0635f064e372a154f99e3a8112648
Anatolii <99074480+AnatoliiMart@users.noreply.github.com>
2023-04-20 19:48:21 +0200

    Добавьте файлы проекта.

---
+++ Library.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+// Создайте приложение для учёта книг. Необходимо хранить следующую информацию:
+
+// Название книги
+// ФИО автора
+// Жанр книги
+// Год выпуска
+
+//Для хранения книг используйте класс LinkedList<T>.
+
+//Приложение должно предоставлять такую функциональность:
+
+// Добавление книг (по одной, массивом, списком)
+// Вывод всего списка книг
+// Вывод книги по номеру в списке
+// Удаление книг
+// Изменение информации о книгах (через индекс)
+// Поиск книг по параметрам
+// Вставка книги в начало списка
+// Вставка книги в конец списка
+// Вставка книги в определенную позицию
+// Удаление книги из начала списка
+// Удаление книги из конца списка
+// Удаление книги из определенной позиции
+
+
+
+
+// Создать меню/подменю пользователя
+namespace ConsoleApp6
+{
+    interface ILibrary
+    {
+        void AddBook(Book book);
+        void AddBook(Book[] books);
+        void AddBook(List<Book> books);
+        void SearchBookByAuthor();
+        void SearchBookByName();
+        void SearchBookByCreationYear();
+        void SearchBookByGenre();
+        void InsrertInBegin();
+        void InsrertInEnd();
+        void InsertByPosition();
+        void RemoveInBegin();
+        void RemoveInEnd();
+        void RemoveByPosition();
+        void ChangeInfo();
+        void ShowAll();
+        void ShowByNumber();
+
+    }
+
+    public class Book : ILibrary
+    {
+        public string bookTitle { get; set; }
+
+        public string authorName { get; set; }
+
+        public string genre { get; set; }
+
+        public int yearOfCreation { get; set; }
+
+        static public LinkedList<Book> books;
+
+        public Book() { }
+
+        public Book(string BookName, string AuthorName, string Genre, int YearOfCreation)
+        {
+          
+            bookTitle = BookName;
+            authorName = AuthorName;
+            genre = Genre;
+            yearOfCreation = YearOfCreation;
+        }
+        void InputData()
+        {
+            Console.Write("Enter book name: ");
+            bookTitle = Console.ReadLine();
+
+            Console.Write("Enter author name: ");
+            authorName = Console.ReadLine();
+
+            Console.Write("Enter genre of book: ");
+            genre = Console.ReadLine();
+
+            Console.Write("Enter year of book creation: ");
+            yearOfCreation = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void AddBook(Book book)
+        {
+            if (books.Count == 0)
+                books.AddFirst(book);
+            else
+                books.AddLast(book);
+        }
+
+        public void AddBook(Book[] Books)
+        {
+            foreach (var item in Books)
+                books.AddLast(item);
+        }
+
+        public void AddBook(List<Book> Books)
+        {
+            foreach (var item in Books)
+                books.AddLast(item);
+        }
+
+        public void ChangeInfo()
+        {
+            Console.WriteLine("Enter index of book what you want to change");
+            int index = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void InsertByPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsrertInBegin()
+        {
+           Book book = new Book();
+            Console.WriteLine("Enter book for insertion: ");
+            book.InputData();
+            books.AddFirst(book);
+        }
+
+        public void InsrertInEnd()
+        {
+            Book book = new Book();
+            Console.WriteLine("Enter book for insertion: ");
+            book.InputData();
+            books.AddLast(book);
+        }
+
+        public void RemoveByPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveInBegin()
+        {
+            if (books.Count <= 0)
+                Console.WriteLine("Nothing to remove");
+            else
+                books.RemoveFirst();
+           
+        }
+
+        public void RemoveInEnd()
+        {
+            if (books.Count <= 0)
+            {
+                Console.WriteLine("Nothing to remove");
+            }
+            else
+            {
+                books.RemoveLast();
+            }
+        }
+
+        public void SearchBookByAuthor()
+        {
+            Console.WriteLine("Enter searching author name:");
+            string? str = Console.ReadLine();
+            var res = from book in books
+                      where book.authorName == str
+                      select book;
+            if (res == null)
+                Console.WriteLine("No results");
+            else
+                foreach (var item in res)
+                    Console.WriteLine(item.ToString());
+        }
+
+        public void SearchBookByCreationYear()
+        {
+            Console.WriteLine("Enter searching book creation year:");
+            int tmp = Convert.ToInt32(Console.ReadLine());
+            var res = from book in books
+                      where book.yearOfCreation == tmp
+                      select book;
+            if (res == null)
+                Console.WriteLine("No results");
+            else
+                foreach (var item in res)
+                    Console.WriteLine(item.ToString());
+        }
+
+        public void SearchBookByGenre()
+        {
+            Console.WriteLine("Enter searching book genre:");
+            string? str = Console.ReadLine();
+            var res = from book in books
+                      where book.genre == str
+                      select book;
+            if (res == null)
+                Console.WriteLine("No results");
+            else
+                foreach (var item in res)
+                    Console.WriteLine(item.ToString());
+        }
+
+        public void SearchBookByName()
+        {
+            Console.WriteLine("Enter searching book name:");
+            string? str = Console.ReadLine();
+            var res = from book in books
+                      where book.bookTitle == str
+                      select book;
+            if (res == null)
+                Console.WriteLine("No results");
+            else
+                foreach (var item in res)
+                    Console.WriteLine(item.ToString());
+        }
+
+        public void ShowAll()
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine(book.ToString());
+            }
+        }
+
+        public void ShowByNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"Book number:\t{books.Count}\t|\tAuthor:\t{authorName}\t|" +
+                   $"\tTitle:\t{bookTitle}\t|\tGenre:\t{genre}\t|\tYear:\t{yearOfCreation}\n";
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System.ComponentModel.Design;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ILibrary library = new Book();
+            Book.books = new LinkedList<Book>();
+            Book[] book =
+            {
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020)
+            };
+            List<Book> books = new List<Book>
+            {
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020),
+                new Book("Programming", "John Doe", "Computer scinse", 2020)
+            };
+            int choser = 0;
+            do
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1.  Add one book\n" +
+                                  "2.  Add array of books\n" +
+                                  "3.  Add list of books\n" +
+                                  "4.  Show all books\n" +
+                                  "5.  Show book by number\n" +
+                                  "6.  Delete book from begin\n" +
+                                  "7.  Delete book from end\n" +
+                                  "8.  Delete book by number\n" +
+                                  "9.  Insert book in begin" +
+                                  "10. Insert book in end" +
+                                  "11. Insert book by index" +
+                                  "12. Change book\n" +
+                                  "0.  Exit");
+                Console.Write("Choose variant: ");
+                choser = Convert.ToInt32(Console.ReadLine());
+                switch (choser)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        library.AddBook(new Book("Programming", "John Doe", "Computer scinse", 2020));
+                        break;
+                    case 2:
+                        library.AddBook(book);
+                        break;
+                    case 3:
+                        library.AddBook(books);
+                        break;
+                    case 4:
+                        library.ShowAll();
+                        break;
+                    case 5:
+                        library.ShowByNumber();
+                        break;
+                    case 6:
+                        library.RemoveInBegin();
+                        break;
+                    case 7:
+                        library.RemoveInEnd();
+                        break;
+                    case 8:
+                        library.RemoveByPosition();
+                        break;
+                    case 9:
+                        library.InsrertInBegin();
+                        break;
+                    case 10:
+                        library.InsrertInEnd();
+                        break;
+                    case 11:
+                        library.InsertByPosition();
+                        break;
+                    case 12:
+                        library.ChangeInfo();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input!!!");
+                        break;
+                }
+            } while (choser > 0);
+            
+
+        }
+    }
+}
